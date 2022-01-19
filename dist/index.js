b778c6a7e99455deea3649bd640045ebb14a0081
Nicola <lanzoni.nicola@gmail.com>
2022-01-19 17:28:55 -0300

    Reorganize the folder

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+function o(r,t){return r*t}function m(r,t){return r+t}export{o as multiply,m as sum};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * This is the main entry point for the js code.
+ */
+
+import { multiply } from "./multiply";
+import { sum } from "./sum";
+
+export { multiply, sum };
